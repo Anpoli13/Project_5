e5edb97634a11ddffb98f9495cd092f7672fc820
Anpoli13 <anpoli13@gmail.com>
2019-11-15 13:42:47 +0200

    Refresh

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.searchPhoto').on('keyup',function(){
         var value = $(this).val().toLowerCase();
         $('a').filter(function(){
-            $(this).toggle($(this).text().toLowerCase().indexOf(value)>-1)
+            $(this).toggle($(this).text().toLowerCase().indexOf(value)>-1);
         });
     });
 });
